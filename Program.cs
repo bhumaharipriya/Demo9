8265730c3d8c5d990f0c06f19a2bcd4306a53af9
bhmaharipriya <bhumaharipriya25@gmail.com>
2021-11-11 16:51:58 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Demo9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+        int y = 20;
+        int z = 30;
+
+        Method(x, y, ref z);
+        Console.WriteLine("x={0}, y={1}, and z={2}", x, y, z);
+    }
+
+    static void Method(int i, int j, ref int k)
+    {
+        i = 100;
+        j = 200;
+        k = 300;
+        }
+    }
+}
